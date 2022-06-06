25e8cce1a6bcdefdfc2c407d2ca9c5d72f560ea7
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-06-06 16:59:18 +0200

    Company App 30.4

---
+++ test/api/departments/delete.test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../../server');
+const Department = require('../../../models/department.model');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('DELETE /api/departments', () => {
+
+  before(async () => {
+    
+    const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
+    await testDepOne.save();
+  
+  });
+  
+  it('/:id should delete and return success', async () =>  {
+
+    const res = await request(server).delete('/api/departments/5d9f1140f10a81216cfd4408');
+    const deletedDep = await Department.findOne({ _id: '5d9f1140f10a81216cfd4408' });
+    expect(res.status).to.be.equal(200);
+    expect(deletedDep).to.be.null;
+
+  });
+
+  after(async () => {
+    await Department.deleteMany();
+  });
+
+});
